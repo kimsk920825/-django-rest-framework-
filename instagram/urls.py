67b15f52e0c396b5936727b24a38e3298d334bf0
kimseungkyubestiedu <kimseungkyubestiedu@gmail.com>
2022-01-07 23:53:59 +0900

    PublicPostListAPIView_PostViewSet

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from . import views
+from django.urls import include,path
+router = DefaultRouter()
+router.register('post',views.PostViewSet) #2개 URL을 만들어준다.
+#router.urls #url pattern list
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,28 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics 
 from django.shortcuts import render
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PublicPostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body:", request.body)#실제 프로덕션이라면 Print는 비추, 장고 기본의 로거를 상요해라
+    #     print("request.POST:", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
+"""
+아래 2개를 한번에 만들어 주는 것이 from rest_framework.viewsets import ModelViewSet, from .serializers import PostSerializer
+def post_list(request):
+    #2개 분기
+    pass
+
+def post_detail(request):
+    # request.method => 최소 3개 분기
+    pass
+"""
