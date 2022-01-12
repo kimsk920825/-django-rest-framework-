693daba4e0ae8e3bc18bf5dec54cc3846050e837
kimseungkyubestiedu <kimseungkyubestiedu@gmail.com>
2022-01-12 15:15:57 +0900

    Fix untracked files

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('instagram.urls')),
-    path('accounts/', include('accounts.urls')),
-]
