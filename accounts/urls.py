72cf93c3932fde3680efccdbbd600c8ea766025e
kimseungkyubestiedu <kimseungkyubestiedu@gmail.com>
2022-01-12 14:59:07 +0900

    Fix untracked files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,5 @@
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 urlpatterns = [
-    
+    path('api-token-auth/', obtain_auth_token),
 ]
--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'accounts',
     'instagram',
 ]
@@ -112,6 +113,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
     'PAGE_SIZE': 3,
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES' :[
